57e9ddad73c7ee3a1d5d164a96db16746ecd75d0
sunell <hs0647@gmail.com>
2026-09-03 12:55:11 +0900

    fix(app): 기본 런타임 메타데이터 연결

--- lib/portfolio/portfolio_app.dart
+++ lib/portfolio/portfolio_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_hesu/portfolio/data/portfolio_data.dart';
 import 'package:portfolio_hesu/portfolio/services/external_launcher.dart';
 import 'package:portfolio_hesu/portfolio/theme/apple_theme.dart';
 import 'package:portfolio_hesu/portfolio/widgets/adaptive_portfolio_shell.dart';
@@ -14,12 +15,16 @@ class PortfolioApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Min He-su Portfolio',
+      title: portfolioData.appTitle,
+      color: const Color(0xFF121316),
       debugShowCheckedModeBanner: false,
       theme: AppleTheme.light(),
       darkTheme: AppleTheme.dark(),
       themeMode: ThemeMode.system,
-      home: AdaptivePortfolioShell(externalLauncher: externalLauncher),
+      home: AdaptivePortfolioShell(
+        data: portfolioData,
+        externalLauncher: externalLauncher,
+      ),
     );
   }
 }
